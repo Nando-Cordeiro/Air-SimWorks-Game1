ec95020490e00e2e6a902d92e9f7503fa0dde1b0
VargentSomnium <bridgesmichael86@gmail.com>
2022-06-06 19:16:44 -0500

    Target Prefab + Gun script
    Target Prefab
    +New material
    +New prefab
    Gun Script
    +New CS script empty
    New Folder & moved arena scripts into it

--- AirSimworksDAWG/Assets/Scripts/Arena(fps)/Gun.cs
+++ AirSimworksDAWG/Assets/Scripts/Arena(fps)/Gun.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Gun : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
