977dc7da6999c736716ec73d95fb7f51bc3eb425
VargentSomnium <bridgesmichael86@gmail.com>
2022-06-08 13:52:28 -0500

    Player Movement
    in recruit

--- AirSimworksDAWG/Assets/Scenes/Recruit/Recruit Scripts/playerController.cs
+++ AirSimworksDAWG/Assets/Scenes/Recruit/Recruit Scripts/playerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public CharacterController characterController;
+    public float speed = 12f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        characterController.Move(move * speed * Time.deltaTime);
     }
 }
