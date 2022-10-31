1a8e2a5637016e150520443896a8aa0754cdb3fc
Ty Victorson <trv17@ymail.com>
2022-10-30 20:23:00 -0500

    arena fix

--- AirSimworksDAWG/Assets/Scripts/FPSGameManager.cs
+++ AirSimworksDAWG/Assets/Scripts/FPSGameManager.cs
@@ -45,6 +45,14 @@ public class FPSGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (player == null)
+        {
+            // todo: check player for local
+            player = FindObjectOfType<Gun>();
+        }
+
+        if (!player.view.IsMine) return;
+
         if (m > -1) s -= Time.deltaTime;
         else
         {
@@ -52,15 +60,10 @@ public class FPSGameManager : MonoBehaviour
             if (!ended) EndGame();
         }
 
-        if (s <= 0f) {
+        if (s <= 0f)
+        {
             m--;
             s = 60;
-        }
-
-        if (player == null)
-        {
-            // todo: check player for local
-            player = FindObjectOfType<Gun>();
         }
 
         if (points >= pointsToNextWeapon)
